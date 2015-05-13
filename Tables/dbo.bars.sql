42143905606aeb8a89826ce8fa0837377eb6bffb
Alex Yates <ajy.mail@gmail.com>
2015-05-13 13:01:23 +0100

    Adding database schema

---
+++ Tables/dbo.bars.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[bars]
+(
+[barType] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[barSize] [int] NULL
+) ON [PRIMARY]
+GO
---
+++ Tables/dbo.foos.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[foos]
+(
+[fooName] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[fooAge] [int] NULL
+) ON [PRIMARY]
+GO
